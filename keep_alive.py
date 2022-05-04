2f1f0869c2f5b4d1c35cc8f945adc5483e52aaae
Calvin Qi <67286641+A109-code@users.noreply.github.com>
2022-05-04 03:08:44 +0000

    Official release version 1.0

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "<p>I did your mom in reeno just to watch you cry I did oyur m,om in xbox live!</p><p> I thlammed my penith in the car door, you slammed your penis in the car door, EeeEeeEeeEeee</p>"
+    return "<p>I did your mom in reeno just to watch you cry I did oyur m,om in xbox live!</p>"
 
 def run():
   app.run(host='0.0.0.0',port=8080)
